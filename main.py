31314a20001a869093b6ded88d12db438aeb8f18
gevar2015 <167052990+gevar2015@users.noreply.github.com>
2024-05-19 09:09:03 +0300

    add class weapon(ABC) and cl S + clB
    Добавили абстрактный класс Оружие и два дочерних класса конкретного оружия (меч и лук)

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ class Weapon(ABC):
     def attack(self):
         """Метод для выполнения атаки, специфичный для каждого оружия."""
         pass
+# Класс меча
+class Sword(Weapon):
+    def attack(self):
+        return "наносит удар мечом"
+
+# Класс лука
+class Bow(Weapon):
+    def attack(self):
+        return "наносит удар из лука"
